624898347d0e1e4434eb3ce886a999df33616f93
Andre Daier <andre.daier@bradesco.com.br>
2025-09-19 19:40:06 -0300

    Streams Collectors e Optional Task 4

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .max((p1, p2) -> Double.compare(p1.getPreco(), p2.getPreco()))
+                .orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream()
+                        .anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        return produtos.stream()
+                .map(produto -> {
+                    if (produto.getCategoria() == CategoriaProduto.ELETRONICO) {
+                        produto.setPreco(produto.getPreco() * 0.85);
+                    }
+                    return produto;
+                })
+                .collect(Collectors.toList());
+    }
+}
