0ffde62ef057527301c33df9502705f1c1ee0682
Andre Daier <andre.daier@bradesco.com.br>
2025-10-03 19:54:54 -0300

    Streams Collectors e Optional - Fixacao T0

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -0,0 +1,47 @@
+import java.util.Locale;
+
+public class Pessoa implements Comparable<Pessoa>{
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        return this.nome.compareTo(outra.nome);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(new Locale("pt", "BR"), "[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
+    }
+}
