58b92a57f57574eb50a942fac91c77e2939932a1
Andre Daier <andre.daier@bradesco.com.br>
2025-10-03 20:07:34 -0300

    Streams Collectors e Optional - Fixacao T1

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                () -> new TreeMap<>(obterComparatorCargo()),
+                Collectors.counting()
+        ));
+    }
+
+    private static Comparator<String> obterComparatorCargo() {
+        List<String> ordem = Arrays.asList("Product Owner", "Analista QA", "Desenvolvedor");
+
+        return Comparator
+                .comparingInt((String s) -> {
+                    int idx = ordem.indexOf(s);
+                    return idx == -1 ? Integer.MAX_VALUE : idx;
+                })
+                .thenComparing(Comparator.naturalOrder());
+    }
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                () -> new TreeMap<String, TreeSet<Pessoa>>(Collections.reverseOrder()),
+                Collectors.toCollection(TreeSet::new)
+        ));
+    }
+}
