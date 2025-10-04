91b9f6744dac9294046d454cb82d841a67cda224
Andre Daier <andre.daier@bradesco.com.br>
2025-10-03 22:38:22 -0300

    Streams Collectors e Optional - Fixacao T4

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                () -> new HashMap<>(),
+                Collectors.groupingBy( Pessoa::getIdade, () -> new TreeMap<>(), Collectors.counting()
+                )
+        ));
+    }
+
+    public static TreeMap<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                () -> new TreeMap<>(obterComparatorCargo()),
+                Collectors.counting()
+        ));
+    }
+
+    private static Comparator<String> obterComparatorCargo() {
+        List<String> ordem = Arrays.asList("Product Owner", "Analista QA", "Desenvolvedor");
+
+        return Comparator
+                .comparingInt((String s) -> {
+                    int idx = ordem.indexOf(s);
+                    return idx == -1 ? Integer.MAX_VALUE : idx;
+                })
+                .thenComparing(Comparator.naturalOrder());
+    }
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                () -> new TreeMap<String, TreeSet<Pessoa>>(Collections.reverseOrder()),
+                Collectors.toCollection(TreeSet::new)
+        ));
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                () -> new HashMap<>(),
+                Collectors.averagingDouble(Pessoa::getSalario)
+        ));
+    }
+
+    public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                () -> new HashMap<>(),
+                Collectors.flatMapping(
+                        p -> p.getHobbies().stream(),
+                        Collectors.toCollection(TreeSet::new)
+                )
+        ));
+    }
+
+}
